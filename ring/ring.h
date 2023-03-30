186d47381f65de3359b3e0d9d778c9482b46de30
Your Name <you@example.com>
2023-03-30 10:51:59 +0200

    Add nix

--- ring/ring.h
+++ ring/ring.h
@@ -15,24 +15,21 @@ template<class T>
 class ring_buf
 {
     public:
-        ring_buf(std::size_t len)
+        ring_buf(std::size_t len = 30)
         {
-            _user_list = std::make_shared<std::list<std::shared_ptr<T>>>();
-            _reuse_list = std::make_shared<std::list<std::shared_ptr<T>>>();
-
             for (int i = 0; i < len; i++)
             {
-                _reuse_list.get()->push_back(std::make_shared<T>());
+                _reuse_list.push_back(std::make_shared<T>());
             }
         }
 
         std::optional<std::shared_ptr<T>> get_new_node()
         {
             check_nodes();
-            if (_reuse_list.get()->size())
+            if (_reuse_list.size())
             {
-                std::shared_ptr<T> node = _reuse_list.get()->front();
-                _reuse_list.get()->pop_front();
+                std::shared_ptr<T> node = _reuse_list.front();
+                _reuse_list.pop_front();
                 return node;
             } else {
                 return {};
@@ -41,15 +38,15 @@ class ring_buf
 
         void push_node(std::shared_ptr<T> node)
         {
-            _user_list.get()->push_back(node);
+            _user_list.push_back(node);
         }
 
         std::optional<std::shared_ptr<T>> pop_node()
         {
-            if(_user_list.get()->size())
+            if(_user_list.size())
             {
-                std::shared_ptr<T> node = _user_list.get()->front();
-                _user_list.get()->pop_front();
+                std::shared_ptr<T> node = _user_list.front();
+                _user_list.pop_front();
                 return node;
             } else {
                 return {};
@@ -58,18 +55,18 @@ class ring_buf
 
         void ruse_node(std::shared_ptr<T> node)
         {
-            _reuse_list.get()->push_back(node);
+            _reuse_list.push_back(node);
         }
 
     private:
-        std::shared_ptr<std::list<std::shared_ptr<T>>> _user_list;
-        std::shared_ptr<std::list<std::shared_ptr<T>>> _reuse_list;
+        std::list<std::shared_ptr<T>> _user_list;
+        std::list<std::shared_ptr<T>> _reuse_list;
 
         void check_nodes()
         {
-            if (!_reuse_list.get()->size())
+            if (!_reuse_list.size())
             {
-                _reuse_list.get()->push_back(std::make_shared<T>());
+                _reuse_list.push_back(std::make_shared<T>());
             }
         }
 };
